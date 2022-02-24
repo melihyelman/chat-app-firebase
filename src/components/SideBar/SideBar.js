b1398af42694bb1d9ae4666e6425f18c8a41eea1
melihyelman <myelman17@gmail.com>
2022-02-25 01:23:20 +0300

    add active channel, userinfo and logout

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import { PlusIcon, MenuIcon, HashtagIcon } from '@heroicons/react/solid'
-import { Link } from 'react-router-dom'
+import { PlusIcon, MenuIcon } from '@heroicons/react/solid'
 import ChannelForm from '../Channels/ChannelForm'
+import ChannelList from '../Channels/ChannelList'
+import Info from '../auth/Info'
 
 const SideBar = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -12,6 +13,7 @@ const SideBar = () => {
     }
     return (
         <>
+
             <div className='bg-gray-700 text-gray-100 md:hidden flex justify-between items-center'>
                 <div className="bg-gradient-to-r from-indigo-600 pl-4 to-yellow-700" style={{ "WebkitBackgroundClip": "text", "WebkitTextFillColor": "transparent" }}>
                     <h3 className='text-2xl font-bold text-center '>Chat.<span className='text-xs'>App</span></h3>
@@ -21,14 +23,13 @@ const SideBar = () => {
                     <MenuIcon className="h-6 w-6" />
                 </button>
             </div>
-            <div className={`bg-blue-800 space-y-6  text-blue-100 w-64 py-6 px-2 absolute inset-y-0 left-0 transform ${!isOpen ? "-translate-x-full" : ""} transition duration-200 ease-in-out md:relative md:translate-x-0`}>
-                <div className='flex py-4 pl-4 pr-2 justify-between items-center text-white transition duration-200 hover:bg-blue-700 rounded-lg' onClick={() => setIsOpenModal(true)}>
+            <div className={`bg-blue-800 text-blue-100 w-64 py-6 px-2 absolute inset-y-0 left-0 transform ${!isOpen ? "-translate-x-full" : ""} transition duration-200 ease-in-out md:relative md:translate-x-0`}>
+                <Info />
+                <div className='flex cursor-pointer py-4 pl-4 pr-2 justify-between items-center text-white transition duration-200 hover:bg-blue-700 rounded-lg' onClick={() => setIsOpenModal(true)}>
                     <span className='text-xl font-extrabold'>Add Chanel</span>
                     <PlusIcon className="h-6 w-6" />
                 </div>
-                <nav>
-                    <Link to={"/"}><a href='#/' className='flex justify-between items-center transition duration-200 hover:bg-blue-700 rounded-lg  py-2.5  px-4'>Chanel 1 <HashtagIcon className='w-4 h-4' /></a></Link>
-                </nav>
+                <ChannelList />
             </div>
             <ChannelForm isOpen={isOpenModal} toggleOpenModal={toggleOpenModal} />
         </>
